8fca5deaa833c3a3266b17dfd50e7e64bd6dcec9
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-07 14:10:40 +0700

    finish api gateway

---
+++ src/main/java/com/example/cau5/Cau5Application.java
@@ -0,0 +1,13 @@
+package com.example.cau5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cau5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cau5Application.class, args);
+    }
+
+}
